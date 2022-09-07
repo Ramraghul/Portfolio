b141bb06aa37a066fa6f496778512a4e6caf5139
Ramraghul <raghulraghul111@gmail.com>
2022-09-07 22:00:40 +0530

    resume and button pending

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -18,7 +18,7 @@ function Project() {
                                     <div className="row" data-aos="fade-up">
                                         <div className="col-lg-12 d-flex justify-content-center">
                                             <ul id="portfolio-flters">
-                                                <li data-filter="" className="filter-active"></li>
+                                                <li data-filter="" className="filter-active">ALl</li>
                                                 {/* <li data-filter=".filter-app">App</li>
                                                 <li data-filter=".filter-card">Card</li>
                                                 <li data-filter=".filter-web">Web</li> */}
--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -7,11 +7,19 @@ function Resume() {
                 <div className="container">
                     <div className="d-flex justify-content-between align-items-center">
                         <main id="main">
-                            <section id="resume" className="resume">
+                        <section id="resume" className="portfolio section-bg">
                                 <div className="container">
+
                                     <div className="section-title">
-                                        <button className="btn btn-danger">Click me</button>
+                                    <h2>Resume</h2>
                                     </div>
+
+                                    <div className="row" data-aos="fade-up">
+                                        <div className="col-lg-12 d-flex justify-content-center">
+                                            
+                                        </div>
+                                    </div>
+
                                 </div>
                             </section>
                         </main>
