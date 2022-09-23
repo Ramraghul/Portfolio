9ec0a1961863f7a3895bd561951963aee24aed5d
Ramraghul <raghulraghul111@gmail.com>
2022-09-23 22:49:15 +0530

    project card

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -10,24 +10,64 @@ function Project() {
                             <section id="portfolio" className="portfolio section-bg">
                                 <div className="container">
 
-                                    <div className="section-title">
+                                    <div className="section-title mb-4">
                                         <h2>Project</h2>
                                         </div>
 
-                                    <div className="row" data-aos="fade-up">
+                                    {/* <div className="row" data-aos="fade-up">
                                         <div className="col-lg-12 d-flex justify-content-center">
                                             <ul id="portfolio-flters">
                                                 <li data-filter=".filter" className="filter-active">ALl</li>
-                                                {/* <li data-filter=".filter-app">App</li>
+                                                <li data-filter=".filter-app">App</li>
                                                 <li data-filter=".filter-card">Card</li>
-                                                <li data-filter=".filter-web">Web</li> */}
+                                                <li data-filter=".filter-web">Web</li>
                                             </ul>
                                         </div>
-                                    </div>
+                                    </div> */}
 
                                     <div className="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
 
-                                        <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+                                        <div className='container'>
+                                            <div className='row main-row'>
+                                                <div className="col-lg-4 col-md-6 mb-5">
+                                                    <div className='card p-3' id='card' style={{width:"16rem"}}>
+                                                        <img src="./img/portfolio/portfolio-1.jpg" alt="forest" className='card-img-top shadow bg-white rounded' />
+                                                        <div className='card-body' id='card-body'>
+                                                            <h5 className='card-title'>Forest</h5>
+                                                            <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eaque.</p>
+                                                            <button className='btn btn-outline-dark'>Live</button>
+                                                            <button className='btn btn-outline-dark'>Github</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="col-lg-4 col-md-6 mb-5">
+                                                    <div className='card p-3' id='card' style={{width:"16rem"}}>
+                                                        <img src="./img/portfolio/portfolio-2.jpg" alt="forest" className='card-img-top shadow bg-white rounded' />
+                                                        <div className='card-body' id='card-body'>
+                                                            <h5 className='card-title'>Forest</h5>
+                                                            <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eaque.</p>
+                                                            <button className='btn btn-outline-dark'>Live</button>
+                                                            <button className='btn btn-outline-dark'>Github</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="col-lg-4 col-md-6 mb-5">
+                                                    <div className='card p-3' id='card' style={{width:"16rem"}}>
+                                                        <img src="./img/portfolio/portfolio-3.jpg" alt="forest" className='card-img-top shadow bg-white rounded' />
+                                                        <div className='card-body' id='card-body'>
+                                                            <h5 className='card-title'>Forest</h5>
+                                                            <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eaque.</p>
+                                                            <button className='btn btn-outline-dark'>Live</button>
+                                                            <button className='btn btn-outline-dark'>Github</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        {/* <div className="col-lg-4 col-md-6 portfolio-item filter-app">
                                             <div className="portfolio-wrap">
                                                 <img src="./img/portfolio/portfolio-1.jpg" className="img-fluid" alt="" />
                                                 <div className="portfolio-links">
@@ -35,60 +75,9 @@ function Project() {
                                                     <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
                                                 </div>
                                             </div>
-                                        </div>
-
-                                        <div className="col-lg-4 col-md-6 portfolio-item filter-web">
-                                            <div className="portfolio-wrap">
-                                                <img src="./img/portfolio/portfolio-2.jpg" className="img-fluid" alt="" />
-                                                <div className="portfolio-links">
-                                                    <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Web 3"><i className="bx bx-plus"></i></a>
-                                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-                                            <div className="portfolio-wrap">
-                                                <img src="./img/portfolio/portfolio-3.jpg" className="img-fluid" alt="" />
-                                                <div className="portfolio-links">
-                                                    <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 2"><i className="bx bx-plus"></i></a>
-                                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div className="col-lg-4 col-md-6 portfolio-item filter-card">
-                                            <div className="portfolio-wrap">
-                                                <img src="./img/portfolio/portfolio-4.jpg" className="img-fluid" alt="" />
-                                                <div className="portfolio-links">
-                                                    <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Card 2"><i className="bx bx-plus"></i></a>
-                                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div className="col-lg-4 col-md-6 portfolio-item filter-web">
-                                            <div className="portfolio-wrap">
-                                                <img src="./img/portfolio/portfolio-5.jpg" className="img-fluid" alt="" />
-                                                <div className="portfolio-links">
-                                                    <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Web 2"><i className="bx bx-plus"></i></a>
-                                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-                                            <div className="portfolio-wrap">
-                                                <img src="./img/portfolio/portfolio-6.jpg" className="img-fluid" alt="" />
-                                                <div className="portfolio-links">
-                                                    <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 3"><i className="bx bx-plus"></i></a>
-                                                    <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
+                                        </div> */}
 
                                     </div>
-
                                 </div>
                             </section>
                         </main>
